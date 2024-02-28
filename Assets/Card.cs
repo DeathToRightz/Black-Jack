faa2ec9c58248bce0d9e001be2648e7cf2ded4b3
DeathToRightz <142912611+DeathToRightz@users.noreply.github.com>
2024-02-28 14:43:02 -0500

    initial creation

---
+++ Assets/Card.cs
@@ -0,0 +1,21 @@
+
+public class Card  
+{
+    private int _cardValue;
+    public Card()
+    {
+
+    }
+
+    public int SetValue
+    {
+        get
+        {
+            return _cardValue;
+        }
+        set
+        {
+            _cardValue = value;
+        }
+    }
+}

03f2664d7a590f4aed4de4b27f15b13e799814b6
DeathToRightz <142912611+DeathToRightz@users.noreply.github.com>
2024-03-01 16:35:11 -0500

    progress

--- Assets/Card.cs
+++ Assets/Card.cs
@@ -1,39 +1,30 @@
 
 using UnityEngine;
-
-public class Card  
+[System.Serializable]
+public class Card
 {
-    private int _cardValue;
-    private Sprite _cardface;
+    int _cardValue;
+    public enum Suit
+    {
+        Clubs = 0,
+        Spaces = 1,
+        Diamonds = 2,
+        Hearts = 3,
+    }
 
+    public Card(Suit suit, int cardValue, Sprite cardFace)
+    {
+        CurrentSuit = suit;
+        Sprite = cardFace;
+        CardValue = cardValue;
+    }
+    public Suit CurrentSuit { get; private set; }
     
-    public Card(int cardValue, Sprite cardFace)
-    {
-        _cardface = cardFace;
-        _cardValue = cardValue;
-    }
+    public Sprite Sprite { get; private set; }
 
-    public int SetValue
+    public int CardValue
     {
-        get
-        {
-            return _cardValue;
-        }
-        set
-        {
-            _cardValue = value;
-        }
-    }
-
-    public Sprite Cardface
-    {
-        get
-        {
-            return _cardface;
-        }
-        set
-        {
-            _cardface = value;
-        }
+        get { return _cardValue; }
+        set { _cardValue = value; }
     }
 }
--- Assets/DeckofCards.cs
+++ Assets/DeckofCards.cs
@@ -1,26 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-
+using static Card;
+//54 cards
 public class DeckofCards : MonoBehaviour
 {
-    [SerializeField] GameObject card1, card2;
-    private Card testCard;
+    [SerializeField] GameObject playerCard1, playerCard2;
+    private List <Card> cards;
     [SerializeField] Sprite[] listOfSprites;
     private SpriteRenderer cardVisual1, cardVisual2;
     void Start()
     {
-        cardVisual1 = card1.GetComponent<SpriteRenderer>();
-        cardVisual1.sprite = listOfSprites[0];
-        /*testCard = new Card(5, listOfSprites[0]);
-        cardVisual = GetComponent<SpriteRenderer>();
-        cardVisual.sprite = listOfSprites[0];*/
+        cards = new List<Card> ();  
+       cardVisual1 = playerCard1.GetComponent<SpriteRenderer> ();
+       // cardVisual1.sprite = listOfSprites[0];
+         CreateDeck();
         
+         Instantiate(playerCard1, playerCard1.transform.position, Quaternion.identity);
+       cardVisual1.sprite = cards[0].Sprite;
+        Debug.Log(cards[0].CardValue);
     }
 
    
-    void Update()
+   
+
+    void CreateDeck()
     {
         
+       
+
+        foreach(Card.Suit suit in Enum.GetValues(typeof(Card.Suit)))
+        {
+            
+           for(int i = 14; i > 1; i--)
+            {
+
+                cards.Add(new Card(suit, i, SetSpriteOnCard()));
+
+            }
+           
+        }
+        
     }
+
+    Sprite SetSpriteOnCard()
+    {
+        return listOfSprites[0];
+    }
+
+    
 }
